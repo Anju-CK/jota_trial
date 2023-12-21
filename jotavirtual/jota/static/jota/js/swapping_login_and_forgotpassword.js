84a3f2f858f8f629197b374a8da994b01b7a1c71
Anju-CK <anju.ck@simelabs.com>
2023-12-21 11:13:13 +0530

    forgot password added

---
+++ jotavirtual/jota/static/jota/js/swapping_login_and_forgotpassword.js
@@ -0,0 +1,7 @@
+function replaceFormWithMessage() {
+        var form = document.getElementById('loginForm');
+        var helloMessage = document.getElementById('helloMessage');
+
+        loginForm.style.display = 'none';
+        forgetpasswordform.style.display = 'block';
+    }
