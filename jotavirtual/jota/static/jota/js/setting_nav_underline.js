0415ff33104bb899effde9b6f2bfbb7e09d7320f
Anju-CK <anju.ck@simelabs.com>
2023-12-21 14:23:11 +0530

    whitelabelling moved to sidebar

--- jotavirtual/jota/static/jota/js/setting_nav_underline.js
+++ jotavirtual/jota/static/jota/js/setting_nav_underline.js
@@ -1,24 +1,24 @@
 document.getElementById('brand').addEventListener('click', function(event) {
     event.preventDefault();
     this.classList.remove('no-underline');
-    document.getElementById('domain').classList.add('no-underline');
+//    document.getElementById('domain').classList.add('no-underline');
     document.getElementById('billing').classList.add('no-underline');
     document.getElementById('management').classList.add('no-underline');
 });
 
-document.getElementById('domain').addEventListener('click', function(event) {
-    event.preventDefault();
-    this.classList.remove('no-underline');
-    document.getElementById('brand').classList.add('no-underline');
-    document.getElementById('billing').classList.add('no-underline');
-    document.getElementById('management').classList.add('no-underline');
-});
+//document.getElementById('domain').addEventListener('click', function(event) {
+//    event.preventDefault();
+//    this.classList.remove('no-underline');
+//    document.getElementById('brand').classList.add('no-underline');
+//    document.getElementById('billing').classList.add('no-underline');
+//    document.getElementById('management').classList.add('no-underline');
+//});
 
 document.getElementById('management').addEventListener('click', function(event) {
     event.preventDefault();
     this.classList.remove('no-underline');
     document.getElementById('billing').classList.add('no-underline');
-    document.getElementById('domain').classList.add('no-underline');
+//    document.getElementById('domain').classList.add('no-underline');
     document.getElementById('brand').classList.add('no-underline');
 });
 
@@ -26,6 +26,6 @@ document.getElementById('billing').addEventListener('click', function(event) {
     event.preventDefault();
     this.classList.remove('no-underline');
     document.getElementById('management').classList.add('no-underline');
-    document.getElementById('domain').classList.add('no-underline');
+//    document.getElementById('domain').classList.add('no-underline');
     document.getElementById('brand').classList.add('no-underline');
 });
